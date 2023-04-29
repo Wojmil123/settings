3450a5f748641d640630185222107a822e454b63
Jerzy Kisielewski <wojmilgamon@gmail.com>
2023-04-29 18:32:55 +0200

    create top and center panel in dialog

---
+++ src/AppSettings.java
@@ -0,0 +1,88 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AppSettings {
+    AppSettings(JFrame frame){
+        JDialog window = new JDialog(frame, "Settings", true);
+        JPanel topPanel, centerPanel, centerPanel1, centerPanel2, bottomPanel;
+        topPanel = new JPanel();
+        setTopPanel(topPanel, window);
+        centerPanel1 = new JPanel();
+        setCenterPanel1(centerPanel1);
+        centerPanel2 = new JPanel();
+        setCenterPanel2(centerPanel2);
+        centerPanel = new JPanel();
+        centerPanel.add(centerPanel1);
+        centerPanel.add(centerPanel2);
+        window.add(topPanel, BorderLayout.NORTH);
+        window.add(centerPanel, BorderLayout.CENTER);
+        setWindow(window);
+    }
+
+    private void setWindow(JDialog dialog){
+        dialog.setMinimumSize(new Dimension(400, 700));
+        dialog.setVisible(true);
+    }
+
+    private void setTopPanel(JPanel panel, JDialog dialog){
+        JButton reject, save;
+        JLabel label = new JLabel("Dostępne ustawienia");
+        reject = new JButton("Odrzuć");
+        setRejectButton(reject, dialog);
+        save = new JButton("Zachowaj");
+        setSaveButton(save, dialog);
+        panel.add(label);
+        panel.add(reject);
+        panel.add(save);
+    }
+
+    private void setRejectButton(JButton button, JDialog dialog){
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dialog.setVisible(false);
+            }
+        });
+    }
+
+    private void setSaveButton(JButton button, JDialog dialog){
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Saved");
+                dialog.setVisible(false);
+            }
+        });
+    }
+
+    private void setCenterPanel1(JPanel panel){
+        panel.setBorder(BorderFactory.createTitledBorder("Okno"));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JCheckBox c1 = new JCheckBox("Uruchom wyśrodkowane");
+        JCheckBox c2 = new JCheckBox("Uruchom zmaksymalizowane");
+        JCheckBox c3 = new JCheckBox("Zapamiętaj pozycję");
+        JCheckBox c4 = new JCheckBox("Zapamiętaj rozmiar");
+        panel.add(c1);
+        panel.add(c2);
+        panel.add(c3);
+        panel.add(c4);
+    }
+
+    private void setCenterPanel2(JPanel panel){
+        panel.setBorder(BorderFactory.createTitledBorder("IDE"));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JComboBox languages = new JComboBox();
+        JLabel label = new JLabel("Język interfejsu");
+        languages.addItem("Polski");
+        languages.addItem("English");
+        languages.addItem("Deutsch");
+        JCheckBox c1 = new JCheckBox("Zapamiętaj katalog przeglądania");
+        JCheckBox c2 = new JCheckBox("Zapamiętaj otwarte pliki");
+        panel.add(label);
+        panel.add(languages);
+        panel.add(c1);
+        panel.add(c2);
+    }
+}
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,17 +1,31 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow {
     MainWindow(){
         JFrame window = new JFrame("MainWindow");
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setVisible(true);
-        window.setSize(600, 400);
+        window.setSize(700, 400);
         JPanel panel = new JPanel();
-        JButton button = new JButton("Settings");
+        JButton button = new JButton("Preferences");
+        setButtonValues(button, window);
         panel.add(button);
         window.add(panel);
     }
 
+    public void setButtonValues(JButton button, JFrame frame){
+        button.setFocusable(false);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("preferences");
+                new AppSettings(frame);
+            }
+        });
+    }
 
     public static void main(String[] args) {
         new MainWindow();
